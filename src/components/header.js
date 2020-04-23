cae76f50587c6a35acada056eca31b2fe8dfb29a
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-04-24 00:27:12 +0200

    header done

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,61 @@
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import "./header.css"
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+import Logo from "../images/lostragos_logo_horizontal.png"
 
-Header.defaultProps = {
-  siteTitle: ``,
+import Menu from "./assets/menu.svg"
+import MenuArrow from "./assets/menu-arrow.svg"
+import Home from "./assets/home.svg"
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  let handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <>
+      <header className="header">
+        <button className="menu-btn" onClick={handleClick}>
+          <Menu className="menu-icon" />
+        </button>
+        <div>
+          <img className="logo" src={Logo} />
+        </div>
+        <button className="search" />
+      </header>
+      <aside>
+        <div className={isOpen ? "menu" : "is-closed"}>
+          <ul>
+            <li className="menu-item">
+              <div className="menu-item-text">ENTRADA</div>
+              <Home className="menu-item-icon" />
+            </li>
+            <li className="menu-item">
+              <div className="menu-item-text">RECETAS</div>
+              <MenuArrow className="menu-item-icon" />
+            </li>
+            <li className="menu-item">
+              <div className="menu-item-text">OCASIONES</div>
+              <MenuArrow className="menu-item-icon" />
+            </li>
+            <li className="menu-item">
+              <div className="menu-item-text">COLECCIONES</div>
+              <MenuArrow className="menu-item-icon" />
+            </li>
+            <li className="menu-item">
+              <div className="menu-item-text">MASTER CLASS</div>
+              <MenuArrow className="menu-item-icon" />
+            </li>
+          </ul>
+        </div>
+      </aside>
+    </>
+  )
 }
 
 export default Header

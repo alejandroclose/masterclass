d81f71b536c9093c4d3c6b3273039550bff7bfa5
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-04-27 20:15:12 +0200

    classbar wip

--- src/components/classbar.js
+++ src/components/classbar.js
@@ -8,6 +8,15 @@ const Classbar = props => {
   return (
     <>
       <div className="classbar">
+        <div className="classbar-content">
+        <div>
+          <div className="classbar-title">MASTER CLASS DE CÓCTELES DEL MUNDO #ENCASA</div>
+          <div>por Cocktail Shop</div>
+        </div>
+        <div>EN VIVO + ONLINE</div>
+        <div>13€</div>
+        <a href="/#inscripción">Inscribirme</a>
+        </div>
       </div>
     </>
   )
--- src/pages/cocktail-shop.js
+++ src/pages/cocktail-shop.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Storefront from "../components/storefront"
+import ClassBar from "../components/classbar"
 
 import Pizarro from "../images/albertopizarro.jpg"
 import CS1 from "../images/cocktailshop1.jpg"
@@ -12,6 +13,7 @@ import CS2 from "../images/cocktailshop2.jpg"
 const CocktailShop = () => (
   <Layout>
     <SEO title="losTRAGOS + Cocktail Shop" />
+    <ClassBar />
     <Storefront featured="https://player.vimeo.com/video/227348480" secondary={Pizarro} galleryone={CS1} gallerytwo={CS2}/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>

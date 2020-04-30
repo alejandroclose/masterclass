6e916602c63f85a4ef7d0a834d300e9fe1b38c53
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-05-01 00:08:37 +0200

    started contentbox

---
+++ src/components/contentbox.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+import {Link} from "gatsby"
+
+import "./contentbox.css"
+
+const Contentbox = props => {
+  // const [, ] = useState();
+
+  return (
+    <>
+    <div className="content-box">adf</div>
+    </>
+  )
+}
+
+export default Contentbox
--- src/pages/cocktail-shop.js
+++ src/pages/cocktail-shop.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo"
 import ClassBar from "../components/classbar"
 import Storefront from "../components/storefront"
 import Features from "../components/features"
+import Contentbox from "../components/contentbox"
 
 import Pizarro from "../images/albertopizarro.jpg"
 import CS1 from "../images/cocktailshop1.jpg"
@@ -17,6 +18,7 @@ const CocktailShop = () => (
     <ClassBar title="MASTER CLASS DE CÓCTELES DEL MUNDO #ENCASA" author="por Cocktail Shop" type="EN VIVO + ONLINE" price="13 €" cta="Inscribirme"/>
     <Storefront featured="https://player.vimeo.com/video/227348480" secondary={Pizarro} galleryone={CS1} gallerytwo={CS2}/>
     <Features time="1 hora" lang="Español" level="Iniciación" format="Online y en vivo" age="18 años" date="sab. 9 mayo 20h (CEST)" />
+    <Contentbox />
   </Layout>
 )
 

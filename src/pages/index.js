4200b4f27ecb4779b6ac8efda7cac3e7e6c8464a
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-05-09 13:56:54 +0200

    fixed button and added changes to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ const IndexPage = () => (
       level="Iniciación"
       format="Online y en vivo"
       age="18 años"
-      date="Sáb. 9 mayo 22h (España)"
+      date="HOY, Sáb. 9 mayo 22h (España)"
       idlink="/cocktail-shop/#horarios"
     />
     <Content title="SOBRE EL CURSO">
@@ -93,7 +93,7 @@ const IndexPage = () => (
       <p>No se admiten cancelaciones.</p>
     </Content>
     <Content id="horarios" title="HORARIOS DISPONIBLES" order="last-content">
-      <Date date="Sáb. 9 mayo 2020" time="22h (CEST, España)" cta="Inscribirme" link="https://zoom.us/webinar/register/WN_LdNskxGxT_WYyiKrxBJOOA"/>
+      <Date date="HOY, Sáb. 9 mayo 2020" time="22h (CEST, España)" cta="Inscribirme" link="https://zoom.us/webinar/register/WN_LdNskxGxT_WYyiKrxBJOOA"/>
     </Content>
   </Layout>
 )
